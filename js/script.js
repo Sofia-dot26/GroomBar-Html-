d1a666fcaac2d303f93688801798202e72e35408
Sofia-dot26 <sofakurerova@gmail.com>
2021-06-07 11:43:23 +0300

    second commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.slider_inner').slick({
+        arrows:false, //стрелки
+        dots: true, //точки
+        slidesToShow:2,
+        slidesToScroll:2,
+        speed: 2000,
+        autoplay:true, 
+        autoplaySpeed:2000
+
+
+
+    });
+
+
+});
